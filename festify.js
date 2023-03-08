f21b1c684a0fca27d3be1f939c759c1e584273f5
Wouter Schut <github@seweso.com>
2023-03-08 18:34:18 +0100

    festify

---
+++ festify.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Example JS Userscript
+// @description This a standard userscript with javascript code
+// @match       https://github.com/quoid/userscripts
+// @exclude-match: *://*.*
+// @version 1.1
+// @updateURL https://www.k21p.com/example.user.js
+// @grant GM.setValue
+// @grant GM.getValue
+// @require https://code.jquery.com/jquery-1.12.4.min.js
+// ==/UserScript==
+
+console.log("I was updated!");
