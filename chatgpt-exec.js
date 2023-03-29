2a3815c14f38e4a2a48027cd4ce7143ece80a161
Wouter Schut <github@seweso.com>
2023-03-29 16:36:42 +0200

    Complete rehaul

--- chatgpt-exec.js
+++ chatgpt-exec.js
@@ -16,68 +16,23 @@ First prompt is: Show a funny quote in an alert box.
 
 */
 
-// Select the node that will be observed for mutations
-const targetNode = document.body;
+// Find the last textarea on the page
+const textareas = document.querySelectorAll('textarea');
+const lastTextarea = textareas[textareas.length - 1];
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true, characterData: true };
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Set to keep track of executed code snippets
-const executedCodeSnippets = new Set();
-
-// Check a node's text content for {exec} and {/exec} tags and execute the code between them
-function checkExecTags(node) {
-  const text = node.textContent;
-  const matches = text.match(/\{exec\}([\s\S]*?)\{\/exec\}/g);
-  if (matches) {
-    matches.forEach(match => {
-      const execText = match.replace(/\{exec\}([\s\S]*?)\{\/exec\}/g, '$1');
-      if (!executedCodeSnippets.has(execText)) {
-        console.log(execText);
-        executedCodeSnippets.add(execText);
-        try {
-          eval(execText);
-        } catch (error) {
-          handleError(error);
-        }
-      }
-    });
-  }
-}
-
-// Callback function to execute when mutations are observed
-function callback(mutationsList, observer) {
-  for (const mutation of mutationsList) {
-    if (mutation.type === 'childList' && mutation.addedNodes.length) {
-      // Check each added node for {exec} and {/exec} tags
-      mutation.addedNodes.forEach(node => {
-        checkExecTags(node);
-      });
-    } else if (mutation.type === 'characterData') {
-      // Check the modified node for {exec} and {/exec} tags
-      checkExecTags(mutation.target);
-    }
-  }
-}
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+// Get the button element based on the last textarea and its next sibling
+const button = lastTextarea.nextElementSibling;
 
 // Send the given prompt text to ChatGPT by setting it as the value of the first textarea and clicking the next sibling button
 function sendPromptToChatGpt(text) {
   setTimeout(() => {
-    const textarea = document.querySelector('textarea');
-    if (textarea) {
-      textarea.value = text;
-      const button = textarea.nextElementSibling;
+    if (lastTextarea) {
+      lastTextarea.value = text;
       if (button.tagName.toLowerCase() === 'button') {
         button.click();
       }
     }
-  }, 1500);
+  }, 500);
 }
 
 // Handle errors caught by try/catch
@@ -86,33 +41,41 @@ function handleError(error) {
   sendPromptToChatGpt(`Error occurred: ${error.message}`);
 }
 
-
 // Function to check if the button is in the "ready" state
-function isButtonReady(button) {
+function isButtonReady() {
   return button.querySelector('svg') !== null;
 }
 
 // Function to handle button state changes
-function onButtonStateChanged(button) {
-  if (isButtonReady(button)) {
+function onButtonStateChanged() {
+  if (isButtonReady()) {
     console.log('Content is ready.');
+
+    // Find the last occurrence of {exec} and {/exec}
+    const execStart = document.body.innerHTML.lastIndexOf('{exec}');
+    const execEnd = document.body.innerHTML.lastIndexOf('{/exec}');
+    if (execStart !== -1 && execEnd !== -1 && execEnd > execStart) {
+      // Extract the text between the last occurrence of {exec} and {/exec}
+      const execText = document.body.innerHTML.substring(execStart + 6, execEnd);
+      console.log(`Text between {exec} and {/exec}: ${execText}`);
+      try {
+        eval(execText);
+      } catch (error) {
+        handleError(error);
+      }
+    } else {
+      console.log('No {exec} block found.');
+    }
   } else {
     console.log('Content is still loading.');
   }
 }
 
-// Find the last textarea on the page
-const textareas = document.querySelectorAll('textarea');
-const lastTextarea = textareas[textareas.length - 1];
-
-// Get the button element based on the last textarea and its next sibling
-const button = lastTextarea.nextElementSibling;
-
 // Create a MutationObserver to watch for changes to the button's content
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (mutation.type === 'childList') {
-      onButtonStateChanged(button);
+      onButtonStateChanged();
     }
   });
 });
@@ -121,4 +84,4 @@ const observer = new MutationObserver((mutations) => {
 observer.observe(button, { childList: true });
 
 // Check the initial state of the button
-onButtonStateChanged(button);
+onButtonStateChanged();
