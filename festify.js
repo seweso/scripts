80f3ff45799a4884553fcfffd0b7e1513b3c2dff
Wouter Schut <github@seweso.com>
2023-03-29 14:55:37 +0200

    Update festify.js

--- festify.js
+++ festify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Festify script
 // @description This enables you to vote multiple times 
-// @match       https://github.com/quoid/userscripts
+// @match       https://festify.us/*
 // @exclude-match: *://*.*
 // @version 1.1
 // @updateURL https://seweso.github.io/scripts/festify.js
@@ -10,5 +10,40 @@
 // @require https://code.jquery.com/jquery-1.12.4.min.js
 // ==/UserScript==
 
-console.log("I was updated!");
-alert("Test 2");
+function deleteFestifyCookies() {
+	document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.festify.us;";
+	document.cookie = "_gid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.festify.us;";
+	localStorage.clear();
+	indexedDB.deleteDatabase("Festify");
+	indexedDB.deleteDatabase("firebaseLocalStorageDb");	
+}
+
+function selectShadowElement(selector, rootNode = document) {
+
+  const element = rootNode.querySelector(selector);
+  if (element) {
+    return element;
+  }
+  
+  const shadowRoots = rootNode.querySelectorAll('*');
+  for (const shadowRoot of shadowRoots) {
+    if (shadowRoot.shadowRoot) {
+      const result = selectShadowElement(selector, shadowRoot.shadowRoot);
+      if (result) {
+        return result;
+      }
+    }
+  }
+  
+  return null;
+}
+
+function addRefreshButton() {
+	const searchBar = selectShadowElement('search-bar');
+	const svgElement = selectShadowElement('svg', searchBar.shadowRoot);
+	svgElement.addEventListener("click", deleteFestifyCookies);
+}
+
+window.onload = addRefreshButton;
+
+
